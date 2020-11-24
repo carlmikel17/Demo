389bcce75eb8f4d60310168afe54e384172b4a65
carlmikel17 <carlmichaelmarin20@gmail.com>
2020-11-24 21:07:19 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,13 +2,7 @@
   	echo "Building the application with Build#: ${BUILD_ID} Version: ${params.VERSION}"
   }
 
- def testApp() {
-   		if (BRANCH_NAME  == 'master' && params.EXECUTE_TEST == true) {
-	  		echo "Testting the application on ${BRANCH_NAME}..."
-   		}
-  }
-
-   def deployApp() {
+ def deployApp() {
   	echo "deploying version ${params.VERSION}"
   }
 
